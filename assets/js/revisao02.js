5e6ded0ad6b54e52637b5e7dc1e90cd746086ea8
Felipe Santos <dev.felipesantos@gmail.com>
2023-05-08 08:38:55 -0300

    feat: add revisao 02 class

---
+++ assets/js/revisao02.js
@@ -0,0 +1,45 @@
+//alert("Hello, Crianças aleatórias!");
+
+// Receber vários dados via prompt e armazenar num array
+
+let entrada;
+let dados = [];
+let contador = 0;
+let msg = "";
+
+// entrada.toUpperCase() == "FIM"
+
+entrada = prompt("Digite um dado ou FIM para encerrar");
+
+// Alimento meu array
+while (entrada != "FIM") {
+  dados.push(entrada);
+  // dados = [entrada] Sobrescreve a primeira posição do array
+  entrada = prompt("Digite um dado ou FIM para encerrar");
+}
+
+// Exibir dados do array
+//console.log(dados);
+
+/* while (contador < dados.length) {
+  console.log(dados[contador]);
+  contador = contador + 1;
+  //contador++;
+} */
+
+/* while (contador < dados.length) {
+  msg = msg + dados[contador] + "\n";
+  //msg += dados[contador] + "\n";
+  contador++;
+} */
+
+/* for (contador; contador < dados.length; contador++) {
+  console.log(dados[contador]);
+  // Após executar o bloco de código,
+  // o laço incrementa e depois verifica
+} */
+
+// Exibir tamanho do array
+//console.log(dados.length);
+
+//console.log(msg);
